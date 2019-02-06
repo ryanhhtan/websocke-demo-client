2fa4f1c25111df601e4f6bf16059350b88a7d96b
ryanhhtan <ryanhhtan@gmail.com>
2019-02-06 09:01:15 -0800

    Updates

---
+++ precache-manifest.0aa352d0f65d51c8a5721c37c612fbdc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3d9d80df709cd43762e",
+    "url": "/websocket-demo-client/static/css/main.f0095a1a.chunk.css"
+  },
+  {
+    "revision": "f3d9d80df709cd43762e",
+    "url": "/websocket-demo-client/static/js/main.f3d9d80d.chunk.js"
+  },
+  {
+    "revision": "5610000becc8306a8401",
+    "url": "/websocket-demo-client/static/js/1.5610000b.chunk.js"
+  },
+  {
+    "revision": "7cb4131d63f9e9b5b85a",
+    "url": "/websocket-demo-client/static/js/runtime~main.7cb4131d.js"
+  },
+  {
+    "revision": "51c9aacef322ea695cdddec27df1093b",
+    "url": "/websocket-demo-client/index.html"
+  }
+];
